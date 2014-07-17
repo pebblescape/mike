2078f0b16d7455cfaeacc945cd2fdffdc9e64577
Kristjan Rang <mail@rang.ee>
2014-07-17 15:49:39 +0300

    Set up test reporting

--- app/spec/spec_helper.rb
+++ app/spec/spec_helper.rb
@@ -1,5 +1,15 @@
 if ENV['CI']
   require 'simplecov'
+  require 'simplecov-rcov'
+  
+  class SimpleCov::Formatter::MergedFormatter
+    def format(result)
+      SimpleCov::Formatter::HTMLFormatter.new.format(result)
+      SimpleCov::Formatter::RcovFormatter.new.format(result)
+    end
+  end
+  
+  SimpleCov.formatter = SimpleCov::Formatter::MergedFormatter
   SimpleCov.start 'rails'
 end
 
