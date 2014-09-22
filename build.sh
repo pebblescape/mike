58714f3874737ed7d15dfc76cfbc65861cab9d20
Kristjan Rang <mail@rang.ee>
2014-09-22 14:58:34 +0300

    Update gems

--- build.sh
+++ build.sh
@@ -2,17 +2,17 @@
 
 cd /vagrant/app
 
+docker rm -f mike-build
+
 echo "-----> Starting redis & postgresql for asset precompile"
 docker start mike-redis mike-postgresql > /dev/null
 
 echo "-----> Building Mike image"
-id=$(git archive master | docker run -d -e DBNAME=docker -e DBUSER=docker -e DBPASS=docker --link mike-redis:redis --link mike-postgresql:db -v /tmp/app-cache:/tmp/cache:rw -a stdin pebbles/pebblerunner build)
-docker attach $id
-test $(docker wait $id) -eq 0
-docker commit $id pebbles/mike > /dev/null
+git archive master | docker run -i -a stdin -a stdout --name mike-build -e DBNAME=docker -e DBUSER=docker -e DBPASS=docker --link mike-redis:redis --link mike-postgresql:db pebbles/pebblerunner build
+# docker commit mike-build pebbles/mike
 
-echo "-----> Pushing Mike image"
+# echo "-----> Pushing Mike image"
 # docker push pebbles/mike
 
-echo "-----> Cleanup"
-docker rm $id
+# echo "-----> Cleanup"
+# docker rm $id
