376a6461c834fe5e2edac13fff1c2713aa3b170d
Kristjan Rang <mail@rang.ee>
2014-10-11 18:30:10 +0300

    Update gems. Remove scripts.

--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker run --rm -i -t -e DBNAME=docker -e DBUSER=docker -e DBPASS=docker -e PORT=5000 -e WORKERS=3 -p 5000:5000 --link mike-redis:redis --link mike-postgresql:db pebbles/mike start
-docker run --rm -i -t -e DBNAME=docker -e DBUSER=docker -e DBPASS=docker -e PORT=5000 -e WORKERS=3 --link mike-redis:redis --link mike-postgresql:db pebbles/mike run bundle exec rails c
