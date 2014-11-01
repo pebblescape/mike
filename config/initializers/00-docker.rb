6b19ad157396213bb7ed149b16ace4ffa0210e24
Kristjan Rang <mail@rang.ee>
2014-11-01 22:24:11 +0200

    Update ruby version and gems

--- config/initializers/00-docker.rb
+++ config/initializers/00-docker.rb
@@ -2,11 +2,11 @@ require 'docker'
 
 # boot2docker configuration
 if ENV['DOCKER_HOST'] && ENV['DOCKER_HOST'].start_with?('tcp:') && ENV['DOCKER_CERT_PATH']
-  ENV['DOCKER_HOST'] = ENV['DOCKER_HOST'].sub('tcp', 'https')
   cert_path = File.expand_path ENV['DOCKER_CERT_PATH']
   Docker.options = {
     client_cert: File.join(cert_path, 'cert.pem'),
-    client_key: File.join(cert_path, 'key.pem')
+    client_key: File.join(cert_path, 'key.pem'),
+    scheme: 'https'
   }
 
   Excon.defaults[:ssl_ca_file] = File.join(cert_path, 'ca.pem')
