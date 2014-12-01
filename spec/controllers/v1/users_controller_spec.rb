42533f327f4c1bd1a48dec9b9c10ef7f935ed78d
Kristjan Rang <mail@rang.ee>
2014-12-01 17:35:52 +0200

    Fix specs

--- spec/controllers/v1/users_controller_spec.rb
+++ spec/controllers/v1/users_controller_spec.rb
@@ -5,19 +5,19 @@ describe V1::UsersController, type: :controller do
 
   context 'auth' do
     it "should return user info by key" do
-      get 'auth', { key: user.ssh_keys.first.key }, api_header
+      authenticated_request :get, 'auth', { key: user.ssh_keys.first.key }
       assert_response 200
       expect(response.body).to include(user.login)
     end
 
     it "should return user info by fingerprint" do
-      get 'auth', { fingerprint: user.ssh_keys.first.fingerprint }, api_header
+      authenticated_request :get, 'auth', { fingerprint: user.ssh_keys.first.fingerprint }
       assert_response 200
       expect(response.body).to include(user.login)
     end
 
     it "should return 404 on invalid key" do
-      get 'auth', { key: 'bla' }, api_header
+      authenticated_request :get, 'auth', { key: 'bla' }
       assert_response 404
       expect(response.body).to include('invalid_ssh_key')
     end
